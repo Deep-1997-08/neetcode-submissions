0574faa7bde9fe749d41d7057dbe4c63af82188b
Deep Bhattacharjee <51170317+Deep-1997-08@users.noreply.github.com>
2026-09-12 17:22:37 -0400

    Add: maximal-square - submission-0

---
+++ Data Structures & Algorithms/maximal-square/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        rows,cols=len(matrix),len(matrix[0])
+        cache={}
+
+        def dfs(r,c):
+            if r>=rows or c>=cols:
+                return 0
+            
+            if (r,c) not in cache:
+                down=dfs(r+1,c)
+                right=dfs(r,c+1)
+                diag=dfs(r+1,c+1)
+
+                cache[(r,c)]=0
+                if matrix[r][c]=="1":
+                    cache[(r,c)]=1+min(down,right,diag)
+            return cache[(r,c)]
+        dfs(0,0)
+        return max(cache.values())**2
